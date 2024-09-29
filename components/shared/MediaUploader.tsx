373e4781177fed66eaa3a664c85a18c47c1eee31
mehrad <mohamadyaghoobi1997@gmail.com>
2024-09-29 06:31:58 +0330

    env example

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="ikarus-artosium.ai"
       options={{
         multiple: false,
         resourceType: "image",
